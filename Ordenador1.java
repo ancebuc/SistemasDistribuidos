b53e0a406bfd02036378fb0cafa581736b304a70
ancebuc <72515749+ancebuc@users.noreply.github.com>
2022-01-11 14:06:20 +0100

    Add files via upload

---
+++ Ordenador1.java
@@ -0,0 +1,60 @@
+package jugadores;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Ordenador1 {
+
+	public static void main(String[] args) {
+		try(Socket s=new Socket("192.168.0.161", 5555);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+				Scanner sc=new Scanner(System.in);){
+			
+			//introducir numero de jugadores
+			System.out.println(reader.readLine());
+			System.out.println(reader.readLine());
+			String numJ=sc.nextLine();
+			writer.write(numJ);
+			writer.newLine();
+			writer.flush();
+			
+			//introducir nombre
+			System.out.println(reader.readLine());
+			writer.write(sc.nextLine());
+			writer.newLine();
+			writer.flush();
+			
+			//Leer y responder
+			String st=reader.readLine();
+			while(!st.contains("fin")) {
+				System.out.println(st);
+				System.out.println(reader.readLine());
+				writer.write(sc.nextLine());
+				writer.newLine();
+				writer.flush();
+				System.out.println(reader.readLine());
+				st=reader.readLine();
+			}
+			
+			//Leer el resultado
+			int numJugadores=Integer.parseInt(numJ);
+			System.out.println(reader.readLine());
+			for(int i=0; i<numJugadores; i++) {
+				System.out.println(reader.readLine());
+				System.out.println(reader.readLine());
+				System.out.println(reader.readLine());
+			}
+		} 
+		
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
